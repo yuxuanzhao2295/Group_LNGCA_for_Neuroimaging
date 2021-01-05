2ee2727f665e128c004f1312318b1a93ce562816
Yuxuan Zhao <38759624+yuxuanzhao2295@users.noreply.github.com>
2021-01-05 18:39:26 -0500

    Update sim_group_comp_extraction.R

--- sim_group_comp_extraction.R
+++ sim_group_comp_extraction.R
@@ -15,19 +15,18 @@ source('fun_dimension_test.R')
 gamma.rate = 1e-4
 gamma.shape = 0.02
 nsub = 20
-nrep = 100
 nrep = 40
 FWHM = 9 
 
 # 
-result_dimest_0715 = array(0, dim = c(nrep,nsub,3,3), dimnames = list(NULL,NULL,SVAR = c('High SVAR', 'Medium SVAR', 'Low SVAR'), 
+result_dimest = array(0, dim = c(nrep,nsub,3,3), dimnames = list(NULL,NULL,SVAR = c('High SVAR', 'Medium SVAR', 'Low SVAR'), 
                                                                   method = c('FOBI-GRF (our)','FOBIasymp','FOBIboot')))
 result = foreach(j = 1:nrep)%dopar%{
   run_dim_est_OneRep(nsub = nsub, gamma.rate = gamma.rate, gamma.shape = gamma.shape, FWHM = FWHM, seed = j, run.method = 1:3)
 }
-for (i in 1:nrep) result_dimest_0715[i,,,] = result[[i]]
-save(result_dimest_0715, file = 'result_dimest_0715.RData')
-apply(result_dimest_0715,c(2,3,4),median)
+for (i in 1:nrep) result_dimest[i,,,] = result[[i]]
+save(result_dimest, file = 'result_dimest.RData')
+#apply(result_dimest,c(2,3,4),mean)
 
 result_correst_3comp = array(0, dim = c(nrep,2,3,3), 
                                   dimnames = list(NULL, method = c('group NGCA', 'group ICA'), 
@@ -35,7 +34,7 @@ result_correst_3comp = array(0, dim = c(nrep,2,3,3),
                                                   Comp = c('Comp 1', 'Comp 2', 'Comp 3')))
 result = foreach(j = 1:nrep) %dopar% {
   run_comp_est_OneRep(nsub = nsub, gamma.rate = gamma.rate, gamma.shape = gamma.shape, FWHM = FWHM, seed = j, 
-                      n.individual.NGCA = result_dimest_0715[j,,,1], n.group=3) # use 3 group components 
+                      n.individual.NGCA = result_dimest[j,,,1], n.group=3) # use 3 group components 
 }
 for (i in 1:nrep) result_correst[i,,,] = result[[i]]
 
